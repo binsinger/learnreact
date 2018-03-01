aa38aca1c3628e02918baf82396d74a7cabb46ee
xiaobin <bin_sing@163.com>
2018-03-01 16:40:32 +0800

    configureStore dev

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -5,3 +5,28 @@ import { logger, router, reduxRouterMiddelware } form '../middleware'
 import rootReducer from '../reducers'
 
 const nextReducer = require('../reducers')
+
+export default function configure(initialStage) {
+	const create = window.devToolsExtension
+		? window.devToolsExtension
+		: createStore
+
+	const createStoreWithMiddleware = applyMiddleware(
+		reduxRouterMiddleware,
+		thunkMiddleware,
+		logger,
+		router,
+	)(create)
+
+	const store = createStoreWithMiddleware(rootReducer, initialState)
+
+	if (module.hot) {
+		module.hot.accept('../reducers', () => {
+			store.replaceReducer(nextReducer)
+		})
+	}
+
+	return store
+}
+
+
